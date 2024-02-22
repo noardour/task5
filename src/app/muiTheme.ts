855ab3b63ee1322e164ac746750169b4ef9ce4d2
Zadorin Artyom <noardour@gmail.com>
2024-02-22 19:51:07 +0500

    create mui theme

---
+++ src/app/muiTheme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const muiTheme = createTheme({
+  palette: {
+    background: {
+      default: "#e8eaeb",
+    },
+  },
+});
+
+export default muiTheme;
--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,15 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Provider } from "react-redux";
 import store from "./app/store.ts";
+import { ThemeProvider } from "@emotion/react";
+import muiTheme from "./app/muiTheme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={muiTheme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
